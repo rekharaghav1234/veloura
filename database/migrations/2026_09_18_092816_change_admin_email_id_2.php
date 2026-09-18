477a29bc617614d5bdb6a8ee9ae80e6675ad5412
Rekha <rekharaghav7017@gmail.com>
2026-09-18 15:01:30 +0530

    Add VELOURA

---
+++ database/migrations/2026_09_18_092816_change_admin_email_id_2.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Facades\DB;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        DB::table('users')
+            ->where('id', 2)
+            ->update([
+                'email' => 'rekharaghav70@gmail.com',
+            ]);
+    }
+
+    public function down(): void
+    {
+        DB::table('users')
+            ->where('id', 2)
+            ->update([
+                'email' => 'raghavrekha5556@gmail.com',
+            ]);
+    }
+};
